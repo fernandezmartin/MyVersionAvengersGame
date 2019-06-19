2d1d2b281f44a12ad9a02096322576511a1cfa31
kiiiu15 <44479717+kiiiu15@users.noreply.github.com>
2019-06-19 20:42:24 -0300

    Adding javadoc to Hero and villain

--- AvengerGameV2/src/game/Hero.java
+++ AvengerGameV2/src/game/Hero.java
@@ -1,5 +1,25 @@
 package game;
-
-public class Hero {
-
+/**
+ * 
+ * @author Agustin Bulzomi
+ * @version 0.1
+ * This class is meant to be use to create objects that are specifications of Character.
+ * It extends from @see Character
+ *
+ */
+public class Hero extends Character {
+		
+	
+	
+	
+	
+	
+	
+		public Hero() {
+			super();
+		}
+		
+		public Hero(String namePased, int lifepased) {
+			super(namePased,lifepased);
+		}
 }
--- AvengerGameV2/src/game/Villain.java
+++ AvengerGameV2/src/game/Villain.java
@@ -1,5 +1,31 @@
 package game;
+/**
+ * 
+ * @author Agustin Bulzomi
+ * @version 0.1
+ * This class is meant to be use to create objects that are specifications of Character.
+ * It extends from @see Character
+ *
+ */
+public class Villain extends Character{
+	private int ambition;
 
-public class Villain {
+	private int getAmbition() {
+		return ambition;
+	}
 
+	private void setAmbition(int ambition) {
+		this.ambition = ambition;
+	}
+	
+	
+	public Villain() {
+		super();
+		setAmbition(0);
+	}
+	
+	public Villain(String namePased, int lifePased, int ambitionPased) {
+		super(namePased, lifePased);
+		setAmbition(ambitionPased);
+	}
 }
