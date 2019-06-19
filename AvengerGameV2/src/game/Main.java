ea39b940c0ed0cc359ee21e6f521baf8ef23e36d
kiiiu15 <44479717+kiiiu15@users.noreply.github.com>
2019-06-19 20:13:59 -0300

    Creation of Main
    add main method and a simple java doc

---
+++ AvengerGameV2/src/game/Main.java
@@ -0,0 +1,17 @@
+package game;
+
+
+/**
+ * 
+ * @author Agustin Bulzomi
+ * This is the main class, this class it's just for makeing the game start
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
