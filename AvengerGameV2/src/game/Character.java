ed1ccc9caa08a2adaa0862a888846b9fd0efba9a
kiiiu15 <44479717+kiiiu15@users.noreply.github.com>
2019-06-19 20:24:37 -0300

    Add java doc to Character class

--- AvengerGameV2/src/game/Character.java
+++ AvengerGameV2/src/game/Character.java
@@ -1,5 +1,39 @@
 package game;
-
+/**
+ * 
+ * @author Agustin Bulzomi
+ * @version 0.1
+ * This class is ment to be container for all villains and hero in the game.
+ *
+ */
 public class Character {
-
+		private String name;
+		private int life;
+		
+		
+		private String getName() {
+			return name;
+		}
+		private void setName(String name) {
+			this.name = name;
+		}
+		private int getLife() {
+			return life;
+		}
+		private void setLife(int life) {
+			this.life = life;
+		}
+		
+		
+		public Character() {
+			setLife(0);
+			setName("");
+		}
+		
+		public Character(String namePassed, int lifePassed) {
+			setLife(lifePassed);
+			setName(namePassed);
+		}
+		
+		
 }
