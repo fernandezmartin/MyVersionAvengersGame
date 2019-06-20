c03b5632886370d7f0466b8c00eb733e9b5ccd56
kiiiu15 <44479717+kiiiu15@users.noreply.github.com>
2019-06-19 23:13:07 -0300

    Creation of basic Stone class

---
+++ AvengerGameV2/src/game/Stone.java
@@ -0,0 +1,52 @@
+package game;
+
+import java.util.Random;
+
+public class Stone {
+	private int minLimit;
+	private int maxLimit;
+	private Random value;
+	private String stoneName;
+	
+	private int getMinLimit() {
+		return minLimit;
+	}
+	private void setMinLimit(int minLimit) {
+		this.minLimit = minLimit;
+	}
+	private int getMaxLimit() {
+		return maxLimit;
+	}
+	private void setMaxLimit(int maxLimit) {
+		this.maxLimit = maxLimit;
+	}
+	private Random getValue() {
+		return value;
+	}
+	private void setValue(Random value) {
+		this.value = value;
+	}
+	private String getStoneName() {
+		return stoneName;
+	}
+	private void setStoneName(String stoneName) {
+		this.stoneName = stoneName;
+	}
+	
+	
+	public Stone() {
+		setMaxLimit(100);
+		setMinLimit(1);
+		setStoneName("");
+		setValue(new Random());
+	}
+	
+	public Stone (String stoneNamePassed, int maxium, int minimun) {
+		setStoneName(stoneNamePassed);
+		setMaxLimit(maxium);
+		setMinLimit(minimun);
+		setValue(new Random());
+	}
+	
+	
+}
